6f9b4e83f0cb28e205432be1fd0a07c18f4a1162
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-06-18 13:14:44 -0300

    aula 02 - Configurando Spring Boot Data JPA
    - Criar tela lista de convidados
    
    - Adicionar boot starter data jpa no POM.xml
    
    - Criar DataSource na classe de configuração;
    
    - Criar entidade convidado
    
    - Criar interface que extend o CRUDRepository;
    
    - Habilitar thymeleaf no html

--- src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -1,14 +1,30 @@
 package br.com.alura.listavip;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.listavip.model.Convidado;
+import br.com.alura.listavip.repository.ConvidadoRepository;
 
 @Controller
 public class ConvidadoController {
+	
+	@Autowired
+	private ConvidadoRepository repository;
 	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
 
+	@RequestMapping("listaconvidados")
+	public String listaConvidados(Model model) {
+		
+		Iterable<Convidado> convidados = repository.findAll(); //busca todos os convidados no banco
+		model.addAttribute("convidados", convidados); //manda a lista para a view
+		
+		return "listaconvidados";
+	}
 }
